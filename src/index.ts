8c06f4df354339edcaaa36676127c3f238c23fd6
Fredrik Andersson <frodrikandersson@gmail.com>
2025-04-04 14:33:52 +0200

    daw

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "https://happy-shop-api.vercel.app",
+  origin: "*",
   credentials: true,  // ✅ Allows cookies
 }));
 
